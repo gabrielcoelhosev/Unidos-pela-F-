0741531b6be82d26c63966b28d962422503e5602
gabrielcoelhosev <gabrielcoelhoseverino6915@gmail.com>
2024-10-02 20:51:34 -0300

    att

--- lib/home_widget.dart
+++ lib/home_widget.dart
@@ -11,20 +11,31 @@ class _HomeWidgetState extends State<HomeWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            height: 300,
-            width: double.infinity,
-            color: Colors.black,
-            child: Center(
-              child: Text(
-                'Unidos pela Fé',
-                style: TextStyle(color: Colors.white),
-              ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.8,
+                  width: MediaQuery.of(context).size.width,
+                  child: Image.asset(
+                    'assets/imgs/capa.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Center(
+                  child: Text('data'),
+                )
+              ],
             ),
-          )
-        ],
+            Container(
+              height: 300,
+              width: double.infinity,
+              color: Colors.black,
+            )
+          ],
+        ),
       ),
     );
   }
